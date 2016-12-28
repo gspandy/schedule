f3918818fd5fe9a5513970a2256d7d028e5e310c
kanven <yuanlong_jiang@sina.com>
2016-12-28 18:17:31 +0800

    任务分发修改

--- schedual-core/src/main/java/com/kanven/schedual/core/JobDispatcher.java
+++ schedual-core/src/main/java/com/kanven/schedual/core/JobDispatcher.java
@@ -4,53 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.Watcher.Event.EventType;
 
 import com.kanven.schedual.core.clustor.Clustor;
 import com.kanven.schedual.core.clustor.impl.DefaultClustorImpl;
+import com.kanven.schedual.register.ChildrenListener;
 import com.kanven.schedual.register.Constants;
 import com.kanven.schedual.register.Register;
-import com.kanven.schedual.register.RegisterException;
 import com.kanven.schedual.transport.client.Client;
 import com.kanven.schedual.transport.client.NettyClient;
 
-public class JobDispatcher {
+public class JobDispatcher implements ChildrenListener {
 
 	private Register register;
 
 	private ExactorConfig config;
 
 	private Clustor clustor = new DefaultClustorImpl();
-
-	private Watcher childrenWatcher = new Watcher() {
-
-		public void process(WatchedEvent event) {
-			EventType type = event.getType();
-			if (EventType.NodeChildrenChanged == type) {
-				init();
-			}
-		}
-
-	};
-
-	private List<String> getChildren() {
-		List<String> dts = new ArrayList<String>();
-		try {
-			List<String> children = register.getChildren(Constants.EXECUTOR_ROOT, childrenWatcher);
-			for (String path : children) {
-				String dt = register.getData(path);
-				if (StringUtils.isEmpty(dt)) {
-					continue;
-				}
-				dts.add(dt);
-			}
-		} catch (RegisterException e) {
-
-		}
-		return dts;
-	}
 
 	private Client createClient(String dt) {
 		String[] items = dt.split(";");
@@ -75,15 +44,7 @@ public class JobDispatcher {
 	}
 
 	public void init() {
-		List<Client> clients = new ArrayList<Client>();
-		List<String> children = getChildren();
-		for (String child : children) {
-			Client client = createClient(child);
-			if (client != null) {
-				clients.add(client);
-			}
-		}
-		clustor.refresh(clients);
+		register.addChildrenListener(Constants.EXECUTOR_ROOT, this);
 	}
 
 	public void alloc(Job job) {
@@ -96,6 +57,17 @@ public class JobDispatcher {
 
 	public void setConfig(ExactorConfig config) {
 		this.config = config;
+	}
+
+	public void handChildrenChange(List<String> children) {
+		List<Client> clients = new ArrayList<Client>();
+		for (String child : children) {
+			Client client = createClient(child);
+			if (client != null) {
+				clients.add(client);
+			}
+		}
+		clustor.refresh(clients);
 	}
 
 }
